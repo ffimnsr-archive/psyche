2c42f47d8fbf975873d0beb847f34fdfc2988e2a
ffimnsr <ffimnsr@gmail.com>
2020-11-09 13:23:15 +0800

    Update and replace npm with yarn

--- src/pages/unused/settings_fragments/SettingsFragment.tsx
+++
@@ -1,5 +0,0 @@
-// Barrel Exports
-export * from "./SettingsFragment_Account";
-export * from "./SettingsFragment_Notifications";
-export * from "./SettingsFragment_PrivacyAndSafety";
-export * from "./SettingsFragment_WorkPreference";
